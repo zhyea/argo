6d925bf110224058f4c02c0a58fdbaeb815bee6f
rui.zhang <rui.zhang@jiduauto.com>
2023-06-26 20:09:28 +0800

    1. 添加用户相关的枚举

--- liens-admin/src/main/java/com/zhyea/biz/liens/LiensAdminApplication.java
+++ liens-admin/src/main/java/com/zhyea/biz/liens/LiensAdminApplication.java
@@ -1,9 +1,6 @@
 package com.zhyea.biz.liens;
 
-import com.fasterxml.jackson.annotation.JsonInclude;
-import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.SerializationFeature;
 import com.zhyea.biz.liens.tools.BeanKit;
 import org.chobit.commons.json.LocalDateTimeModule;
 import org.chobit.commons.utils.JsonKit;
@@ -38,16 +35,7 @@ public class LiensAdminApplication {
 
     @Bean
     public ObjectMapper mapper() {
-        ObjectMapper mapper = new ObjectMapper();
-        mapper.configure(DeserializationFeature.FAIL_ON_IGNORED_PROPERTIES, false);
-        mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
-        mapper.configure(DeserializationFeature.FAIL_ON_NULL_FOR_PRIMITIVES, false);
-        mapper.configure(DeserializationFeature.FAIL_ON_INVALID_SUBTYPE, false);
-        mapper.configure(SerializationFeature.FAIL_ON_EMPTY_BEANS, false);
-
-        mapper.setSerializationInclusion(JsonInclude.Include.NON_DEFAULT);
-        mapper.setSerializationInclusion(JsonInclude.Include.NON_NULL);
-        mapper.setSerializationInclusion(JsonInclude.Include.NON_EMPTY);
+        ObjectMapper mapper = JsonKit.mapper();
 
         mapper.registerModule(new LocalDateTimeModule());
 
