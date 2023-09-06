8f00a0ac9c8b8353cbea4ab9754cc8ac0b848a56
zhyea <zhangrui@zhyea.com>
2023-09-06 20:52:58 +0800

    1. 添加CMS模块

--- liens-admin/src/main/java/com/zhyea/biz/liens/LiensAdminApplication.java
+++ liens-admin/src/main/java/com/zhyea/biz/liens/LiensAdminApplication.java
@@ -11,7 +11,7 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 
 /**
- * 启动类
+ * Liens Admin 启动类
  *
  * @author robin
  */
---
+++ liens-cms/src/main/java/com/zhyea/biz/liens/cms/LiensCmsApplication.java
@@ -0,0 +1,9 @@
+package com.zhyea.biz.liens.cms;
+
+/**
+ * Liens CMS 启动类
+ *
+ * @author rui.zhang
+ */
+public class LiensCmsApplication {
+}
