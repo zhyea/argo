2fb4fc4f55369ca8cb20b6ea2b18cfd635b631e5
zhyea <zhangrui@zhyea.com>
2023-09-08 00:04:13 +0800

    1. 调整response返回值
    Change-Id: Icbcaa73d4ea99cada7b238113489d938e032ec58

---
+++ liens-cms/src/main/java/com/zhyea/biz/liens/cms/config/ResponseWrapperBody.java
@@ -0,0 +1,19 @@
+package com.zhyea.biz.liens.cms.config;
+
+
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.lang.annotation.*;
+
+/**
+ * @author robin
+ */
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ElementType.TYPE, ElementType.METHOD})
+@Documented
+@ResponseBody
+public @interface ResponseWrapperBody {
+
+
+
+}
