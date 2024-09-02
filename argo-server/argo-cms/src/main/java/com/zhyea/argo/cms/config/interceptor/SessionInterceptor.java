90a125bb8755a7f588e3836821adb891bf44d116
zhyea <zhangrui@zhyea.com>
2024-09-02 23:42:18 +0800

    1. 补充数据定义
    Change-Id: I378136cd0167fa68524265a7b966d1a5f7c15b10

--- argo-server/argo-cms/src/main/java/com/zhyea/argo/cms/config/interceptor/SessionInterceptor.java
+++ argo-server/argo-cms/src/main/java/com/zhyea/argo/cms/config/interceptor/SessionInterceptor.java
@@ -6,6 +6,8 @@ import org.springframework.web.servlet.ModelAndView;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import static org.chobit.commons.utils.StrKit.uuid;
+
 /**
  * session信息拦截器
  *
@@ -17,9 +19,8 @@ public class SessionInterceptor implements HandlerInterceptor {
 	@Override
 	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
 			throws Exception {
-
-		System.out.println(Thread.currentThread().getId() + " ------------------BEGIN " + request.getRequestedSessionId());
-
+		String sessionId = request.getSession(true).getId();
+		System.out.println(Thread.currentThread().getId() + " ------------------BEGIN " + sessionId);
 		return true;
 	}
 
@@ -35,7 +36,7 @@ public class SessionInterceptor implements HandlerInterceptor {
 	@Override
 	public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex)
 			throws Exception {
-
+		response.setHeader("sessionId", uuid());
 		System.out.println(Thread.currentThread().getId() + " ------------------END  " + request.getRequestedSessionId());
 	}
 }
