4318308b1b55feaa706d7a5d34eddf5636d74cb3
zhyea <zhangrui@zhyea.com>
2024-10-23 08:49:05 +0800

    1. 添加路由meta
    Change-Id: I57da6fd9214fa80d293a2b0c8a13f4d2266af2e7

--- argo-front/src/view/app/routes.js
+++ argo-front/src/view/app/routes.js
@@ -5,9 +5,15 @@ import AppEditPage from './AppEdit.vue'
 export default [{
 	name: 'AppList',
 	path: '/app-list',
-	component: AppListPage
+	component: AppListPage,
+	meta: {
+		title: '应用列表',
+	},
 }, {
 	name: 'AppAdd',
 	path: '/app-add',
-	component: AppEditPage
+	component: AppEditPage,
+	meta: {
+		title: '新增应用',
+	},
 }]
