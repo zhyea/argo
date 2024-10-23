a9d0a526311ad1dd7257e2ae2f423b81b1f267e0
rui.zhang <rui.zhang@jiduauto.com>
2024-10-23 18:02:57 +0800

    1. 应用编辑功能开发

--- argo-front/src/api/app.js
+++ argo-front/src/api/app.js
@@ -1,6 +1,34 @@
 import request from '@/utils/request.js'
 
-
+// 获取应用列表
 export function getAppList(){
 	return request.get('/cms/app/all')
+}
+
+
+// 获取应用详情
+export function getApp(appId){
+	return request.get('/cms/app/get', {
+		appId: appId,
+	})
+}
+
+
+// 新增应用记录
+export function addApp(formData){
+	return request.post('/cms/app/add',formData)
+}
+
+
+// 编辑应用记录
+export function editApp(formData){
+	return request.post('/cms/app/modify',formData)
+}
+
+
+// 删除应用记录
+export function delApp(appId){
+	return request.post('/cms/app/delete', {
+		appId: appId,
+	})
 }
--- argo-front/src/view/app/AppEdit.vue
+++ argo-front/src/view/app/AppEdit.vue
@@ -1,12 +1,16 @@
 <template>
 	<!--表单信息-->
 	<el-form
-	         :model="appForm" ref="appFormRef" :rules="appFormRules"
-	         label-suffix=":" label-width="90px"
-	         class="app-form" status-icon >
+		:model="appForm" ref="appFormRef" :rules="appFormRules"
+		label-suffix=":" label-width="90px"
+		class="app-form" status-icon>
 
-		<el-form-item label="应用ID" prop="appId">
-			<el-input v-model="appForm.appId"/>
+		<el-form-item prop="appId">
+			<el-input type="hidden" v-model="appForm.id"/>
+		</el-form-item>
+
+		<el-form-item label="应用ID" prop="appCode">
+			<el-input v-model="appForm.appCode"/>
 		</el-form-item>
 
 		<el-form-item label="应用名称" prop="appName">
@@ -22,16 +26,19 @@
 		</el-form-item>
 
 		<el-form-item>
-			<el-button type="primary" @click="onSubmit">提交</el-button>
+			<el-button type="primary" @click="submitAppForm">提交</el-button>
 		</el-form-item>
 	</el-form>
 </template>
 
 <script setup>
 import {ref} from "vue";
+import {addApp, editApp} from "@/api/app.js";
+import {ElMessage} from "element-plus";
 
 // app 表单数据
 const appForm = ref({
+	appId: '',
 	appCode: '',
 	appName: '',
 	icon: '',
@@ -40,12 +47,12 @@ const appForm = ref({
 
 
 // app 表单引用
-const appFormRef = ref({})
+const appFormRef = ref()
 
 
 // app 表单验证规则
 const appFormRules = {
-	appId: [
+	appCode: [
 		{required: true, message: '请输入应用ID', trigger: 'blur'},
 	],
 	appName: [
@@ -54,9 +61,41 @@ const appFormRules = {
 }
 
 
-// 提交app编辑信息
-function onSubmit() {
+// 标记是否已经提交表单
+const isAppFormSubmitted = ref(false)
 
+
+// 提交app编辑信息
+function submitAppForm() {
+	const formData = {...appForm.value}
+	let maintainMethod = addApp
+
+	if (formData.id) {
+		maintainMethod = editApp
+	}
+
+	maintainApp(maintainMethod, formData)
+}
+
+
+// 维护应用信息
+const maintainApp = (maintainMethod, formData) => {
+	appFormRef.value.validate((valid) => {
+		if (!valid) return
+
+		isAppFormSubmitted.value = true
+
+		maintainMethod(formData, maintainMethod).then(response => {
+			if (response.data) {
+				ElMessage.success({
+					message: '保存成功',
+					duration: 1500,
+				})
+			}
+		}).catch(() => {
+			isAppFormSubmitted.value = false
+		})
+	})
 }
 
 </script>
