fc3e4647b552922611578838e54ed3a412e18d9e
zhyea <zhangrui@zhyea.com>
2024-11-08 23:04:34 +0800

    1. 调整组件实例抽屉
    Change-Id: Ib07fa49bee4029420c357ab290ad0e9a53c5e405

--- argo-front/src/view/fci/FciDrawer.vue
+++ argo-front/src/view/fci/FciDrawer.vue
@@ -1,11 +1,11 @@
 <template>
 	<el-drawer title="编辑组件实例"
-	           v-model="fciItemDrawer" :with-header=true size="40%">
+	           v-model="fciItemDrawer" :with-header=true size="50%">
 		<el-container>
 			<!--表单信息-->
 			<el-form status-icon
 			         label-position="right"
-			         label-width="90px"
+			         label-width="100px"
 			         label-suffix=":"
 			         :model="fciForm" ref="fciFormRef" :rules="fciFormRules" class="fci-form">
 
@@ -13,6 +13,44 @@
 					<el-input type="hidden" v-model="fciForm.fcmId"/>
 				</el-form-item>
 
+				<el-form-item label="名称" prop="name">
+					<el-input id="name" v-model="fciForm.name"/>
+				</el-form-item>
+
+				<el-form-item label="是否启用" prop="switchFlag">
+					<el-switch id="switchFlag" v-model="fciForm.switchFlag"
+					           inline-prompt size="large"
+					           active-text="是" :active-value="1"
+					           inactive-text="否" :inactive-value="0"
+					/>
+				</el-form-item>
+
+				<el-form-item label="生效周期" prop="effectivePeriodType">
+					<el-radio-group id="effectivePeriodType" v-model="fciForm.effectivePeriodType">
+						<el-radio v-for="e in effectivePeriodTypeEnum"
+						          :value="e[0]">
+							{{ e[1] }}
+						</el-radio>
+					</el-radio-group>
+				</el-form-item>
+
+				<el-form-item label="生效开始时间" prop="effectiveStartTime">
+					<el-date-picker id="effectiveStartTime" v-model="fciForm.effectiveStartTime"
+					                type="datetime"
+					                placeholder="请选择生效开始时间"
+					                value-format="YYYY-MM-DD HH:mm:ss"/>
+				</el-form-item>
+
+				<el-form-item label="生效结束时间" prop="effectiveEndTime">
+					<el-date-picker id="effectiveEndTime" v-model="fciForm.effectiveEndTime"
+					                type="datetime"
+					                placeholder="请选择生效结束时间"
+					                value-format="YYYY-MM-DD HH:mm:ss"/>
+				</el-form-item>
+
+				<el-form-item label="备注" prop="remark">
+					<el-input id="remark" type="textarea" v-model="fciForm.remark" :autosize="{ minRows: 4,}"/>
+				</el-form-item>
 			</el-form>
 		</el-container>
 	</el-drawer>
@@ -25,15 +63,22 @@ import {ref} from "vue";
 const fciItemDrawer = ref(false)
 
 const fciForm = ref({
-	id: '',
+	id: 0,
+	fcmId: 0,
+	name: '',
+	dataUrl: '',
+	switchFlag: 0,
+	effectivePeriodType: 0,
+	effectiveStartTime: '',
+	effectiveEndTime: '',
 	remark: '',
 })
 
 const fciFormRef = ref()
 
 const fciFormRules = {
-	ruleTitle: [
-		{required: true, message: '请输入规则名称', trigger: 'blur'},
+	name: [
+		{required: true, message: '请输入组件名称', trigger: 'blur'},
 	],
 	response: [
 		{required: true, message: '请输入返回值', trigger: 'blur'},
@@ -65,6 +110,9 @@ const openFciDrawer = (fcmId, fciId) => {
 
 
 defineExpose({openFciDrawer})
+
+
+const effectivePeriodTypeEnum = ref([])
 
 
 </script>
