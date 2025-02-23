c6577988c5466825141754392973d56e062ae90c
zhyea <zhangrui@zhyea.com>
2025-02-23 08:25:16 +0800

    1. 整理页面接口
    Change-Id: Iaf07270faa17731527d0c8446d44772b76b0f286

---
+++ argo-front/src/api/page.js
@@ -0,0 +1,44 @@
+import request from "@/utils/request.js";
+
+
+// 新增应用页面
+export function addPage(pageData) {
+	return request.post('/cms/app/page/add', pageData)
+}
+
+
+// 编辑应用页面
+export function editPage(pageData) {
+	return request.post('/cms/app/page/edit', pageData)
+}
+
+
+// 获取应用页面信息
+export function getPage(pageId) {
+	return request.post('/cms/app/page/get', {
+		pageId: pageId,
+	})
+}
+
+
+// 删除应用页面
+export function deletePage(pageId) {
+	return request.post('/cms/app/page/delete', {
+		pageId: pageId,
+	})
+}
+
+
+// 根据应用ID获取页面列表
+export function findPages(appId, keyword) {
+	return request.post('/cms/app/page/list', {
+		appId: appId,
+		keyword: keyword,
+	})
+}
+
+
+// 生成页面代码
+export function generatePageCode() {
+	return request.get('/cms/app/page/page-code')
+}
