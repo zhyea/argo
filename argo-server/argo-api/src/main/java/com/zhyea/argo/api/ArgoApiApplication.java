f0f05c10134c059012bf2f46d59df1c2ab990972
zhyea <zhangrui@zhyea.com>
2025-05-06 22:53:32 +0800

    1. 添加API模块
    Change-Id: I24d5c686dfc8ebaf1518a2110a761e90959bb8e5

---
+++ argo-server/argo-api/src/main/java/com/zhyea/argo/api/ArgoApiApplication.java
@@ -0,0 +1,27 @@
+package com.zhyea.argo.api;
+
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+
+/**
+ * Argo CMS 启动类
+ *
+ * @author robin
+ */
+//@ComponentScan("com.zhyea.argo")
+@Slf4j
+//@EnableCaching
+//@SpringBootApplication
+public class ArgoApiApplication {
+
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(ArgoApiApplication.class, args);
+
+		logger.info("应用 ARGO API 启动成功 ...... ");
+	}
+
+
+}
