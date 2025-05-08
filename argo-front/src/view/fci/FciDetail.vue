99be4218bc3fe936efa94f6d22c6c633d8d67caf
zhyea <zhangrui@zhyea.com>
2025-05-08 23:20:27 +0800

    1. 准备组件实例详情页面-5
    Change-Id: Iff838f255af694a0c1ee19003481638927f388e5

--- argo-front/src/view/fci/FciDetail.vue
+++ argo-front/src/view/fci/FciDetail.vue
@@ -7,35 +7,35 @@
 			<el-aside class="left_container">
 				<el-row class="item-row">
 					<el-col :span="6" class="item-label">组件名称</el-col>
-					<el-col :span="18" class="item-value">{{name}}</el-col>
+					<el-col :span="18" class="item-value">{{fciData.name}}</el-col>
 				</el-row>
 				<el-row class="item-row">
 					<el-col :span="6" class="item-label">组件代码</el-col>
-					<el-col :span="18" class="item-value">{{fciCode}}</el-col>
+					<el-col :span="18" class="item-value">{{fciData.fciCode}}</el-col>
 				</el-row>
 				<el-row class="item-row">
 					<el-col :span="6" class="item-label">模型名称</el-col>
-					<el-col :span="18" class="item-value">{{fcmName}}</el-col>
+					<el-col :span="18" class="item-value">{{fciData.fcmName}}</el-col>
 				</el-row>
 				<el-row class="item-row">
 					<el-col :span="6" class="item-label">组件类型</el-col>
-					<el-col :span="18" class="item-value">{{typeName}}</el-col>
+					<el-col :span="18" class="item-value">{{fciData.typeName}}</el-col>
 				</el-row>
 				<el-row class="item-row">
 					<el-col :span="6" class="item-label">是否绑定数据</el-col>
-					<el-col :span="18" class="item-value">{{dataBindFlag}}</el-col>
+					<el-col :span="18" class="item-value">{{fciData.dataBindFlag>0  ? '是' : '否'}}</el-col>
 				</el-row>
-				<el-row class="item-row" v-if="dataBindFlag">
+				<el-row class="item-row" v-if="fciData.dataBindFlag">
 					<el-col :span="6" class="item-label">数据连接</el-col>
-					<el-col :span="18" class="item-value">{{dataUrl}}</el-col>
+					<el-col :span="18" class="item-value">{{fciData.dataUrl}}</el-col>
 				</el-row>
 				<el-row class="item-row">
 					<el-col :span="6" class="item-label">生效周期</el-col>
-					<el-col :span="18" class="item-value">{{effectivePeriod}}</el-col>
+					<el-col :span="18" class="item-value">{{fciData.effectivePeriod}}</el-col>
 				</el-row>
 				<el-row class="item-row">
 					<el-col :span="6" class="item-label">备注</el-col>
-					<el-col :span="18" class="item-value">{{remark}}</el-col>
+					<el-col :span="18" class="item-value">{{fciData.remark}}</el-col>
 				</el-row>
 			</el-aside>
 
@@ -54,24 +54,46 @@ import {loadEnums} from "@/api/common.js";
 import {config} from "@/config/index.js";
 import {ElMessage} from "element-plus";
 import FciDrawer from "@/view/fci/FciEditDrawer.vue";
-import {delFci, findFciList} from "@/api/fci.js";
+import {delFci, findFciList, getFci} from "@/api/fci.js";
 import Sidebar from "@/component/layout/SideBar.vue";
 import HeadBar from "@/component/layout/HeadBar.vue";
 
 
 const route = useRoute();
-const router = useRouter();
 
-// 分页数据
-const pageData = ref({
-	// 当前页
-	pageNo: 1,
-	// 页面长度
-	pageSize: 10,
-	// 记录总数
-	total: 0,
+const fciData = ref({
+	id: 0,
+	fcmId: 0,
+	appId: 0,
+	name: '',
+	fciCode: '',
+	fcmName: '',
+	typeName: '',
+	dataBindFlag: 0,
+	dataUrl: '',
+	switchFlag: 1,
+	effectivePeriodType: 1,
+	effectiveStartTime:'',
+	effectiveEndTime: '',
+	remark: '',
 })
 
+// 加载组件实例数据
+const loadFciData = async () => {
+	const fciId = route.params.fciId;
+
+	getFci(fciId).then(response => {
+		if (response && response.data) {
+			fciData.value = response.data;
+		}
+	})
+}
+
+
+onMounted(() => {
+	// 加载页面数据
+	loadFciData()
+})
 
 </script>
 
