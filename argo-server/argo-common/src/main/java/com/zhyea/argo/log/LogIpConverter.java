6b734a11f9273d0175b8407fd75002b6f442bd1c
zhyea <zhangrui@zhyea.com>
2025-05-10 23:04:37 +0800

    1. 优化日志
    Change-Id: I3efc5aff7bc241b96062ac3b978d4c9b562c359f

---
+++ argo-server/argo-common/src/main/java/com/zhyea/argo/log/LogIpConverter.java
@@ -0,0 +1,30 @@
+package com.zhyea.argo.log;
+
+import ch.qos.logback.classic.pattern.ClassicConverter;
+import ch.qos.logback.classic.spi.ILoggingEvent;
+
+import java.net.InetAddress;
+
+/**
+ * 日志IP获取
+ *
+ * @author robin
+ * @since 2025/5/10 13:17
+ */
+public class LogIpConverter extends ClassicConverter {
+
+	private String ip = null;
+
+	@Override
+	public String convert(ILoggingEvent iLoggingEvent) {
+		if (null == this.ip) {
+			try {
+				ip = InetAddress.getLocalHost().getHostAddress();
+			} catch (Exception e) {
+				return "";
+			}
+		}
+
+		return this.ip;
+	}
+}
