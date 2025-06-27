1f9ad38ffea4bc0941e7d9d2ab73d33d0d85181e
zhyea <zhangrui@zhyea.com>
2025-06-27 23:16:46 +0800

    1. 准备定义一个枚举表单组件
    Change-Id: I1f55228fd9e400bb53860da71bd50f4a29788374

---
+++ argo-front/src/component/form/EnumView.vue
@@ -0,0 +1,33 @@
+<template>
+
+	<el-radio-group id="effectivePeriodType" v-if="props.type==='radio'" v-model="fciForm.effectivePeriodType">
+		<el-radio v-for="e in enumRef"
+		          :value="e[0]">
+			{{ e[1] }}
+		</el-radio>
+	</el-radio-group>
+
+
+</template>
+
+<script setup lang="ts">
+import {computed} from "vue";
+import {useEnumStore} from "@/store/enum";
+
+const props = defineProps({
+	id: String,
+	type: String,
+	enum: String,
+})
+
+
+const enumStore = useEnumStore();
+
+const enumRef = computed(() => {
+	return enumStore.getEnumMap(props.enum!);
+})
+</script>
+
+<style scoped lang="less">
+
+</style>
